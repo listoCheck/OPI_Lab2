6159b4f4e841849122c9842e187073f8a107cad7
artem <v.amuzinsky@yandex.ru>
2025-03-02 17:21:18 +0300

    r4

--- Software Engineering Basics Commit/J.java
+++ Software Engineering Basics Commit/J.java
@@ -1,6 +1,38 @@
-public interface J {
+public class J implements C, E {
 
-    String nn();
+    private double c = 100.500;
 
-    double ad();
+    private String e = "test";
+
+    public java.util.List<String> jj() {
+        return new java.util.LinkedList<String>();
+    }
+
+    public Object pp() {
+        return this;
+    }
+
+    public String kk() {
+        return "Yes";
+    }
+
+    public int[] ii() {
+        return new int[]{4, 3, 2, 1};
+    }
+
+    public float ff() {
+        return 0;
+    }
+
+    public void aa() {
+        System.out.println("Hello world!");
+    }
+
+    public long dd() {
+        return 100500;
+    }
+
+    public double ad() {
+        return 9.11;
+    }
 }
